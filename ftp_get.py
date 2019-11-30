1608fa6f17d7a3a7895e5cbdbfb5177504a9dc20
nehalb123 <nehal.borole@gmail.com>
2019-11-30 09:54:33 +0530

    A Star Navigation

--- ftp_get.py
+++
@@ -1,9 +0,0 @@
-import ftplib
-session = ftplib.FTP('192.168.1.114','ubuntu','ubuntu')   
-file = open('/home/nehal/home.db','wb')       
-print(session.pwd())
-print(ftplib.FTP.dir(session))
-session.cwd('/home/ubuntu/.ros/')
-session.retrbinary('RETR rtabmap.db', file.write)   #fetch file from
-file.close()
-session.quit()
